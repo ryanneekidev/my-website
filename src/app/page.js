0af00aa35a9427b5e7a1fe04b9fde3d2d33acbd3
Rayan Mamhoud <kiraclover@protonmail.com>
2025-06-26 14:41:12 +0100

    Add background

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-screen w-screen bg-[url(../../public/Gradient.png)]">
       welcome to my website!
     </div>
   );
