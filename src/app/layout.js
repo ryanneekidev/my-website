9b281e28bf846515c135d32eef6d6df95aa07611
Rayan Mamhoud <kiraclover@protonmail.com>
2025-06-26 14:47:31 +0100

    Build default layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,13 @@ import { inter } from "./ui/fonts";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased h-screen w-screen bg-[url(../../public/Gradient.png)]`}>
-        {children}
+      <body className={`${inter.className} antialiased h-screen w-screen bg-[url(../../public/Gradient.png)] grid grid-cols-[1fr] grid-rows-[4rem_1fr]`}>
+        <div className="row-start-1 row-end-2 col-start-1 col-end-2 bg-pink-500 text-white">
+          navbar
+        </div>
+        <div className="row-start-2 row-end-3 col-start-1 col-end-2">
+          {children}
+        </div>
       </body>
     </html>
   );
