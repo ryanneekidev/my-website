3c89b0993c4e502141d33a112e5a6c94bd8e2c12
Rayan Mamhoud <kiraclover@protonmail.com>
2025-06-29 18:08:34 +0100

    Update navbar

--- src/app/ui/navbar.js
+++ src/app/ui/navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar(){
     return (
         <>
-            <div className="navbar w-full h-[60px] rounded-full flex justify-center items-center backdrop-blur-sm border-1 border-white/10">
+            <div className="navbar bg-sky-200/5  w-full h-[60px] rounded-full flex justify-center items-center backdrop-blur-sm border-1 border-white/10">
                 <a className="text-xl relative text-white hover:cursor-pointer group mx-[1.5rem]">
                     <span className="z-10 group-hover:cursor-pointer">About</span>
                     <div className="bg-indigo-400 rounded-full w-[50%] absolute h-[50%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 blur-lg opacity-0 group-hover:opacity-100 transition-opacity ease-in-out">.</div>
