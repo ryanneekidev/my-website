53103c0ffedec593eced02e56cad3fd7ced94826
ryanneeki <kiraclover@protonmail.com>
2025-06-30 20:26:24 +0100

    Fixed background

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import Navbar from "./ui/navbar";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased bg-[url(../../public/Gradient.png)]`}>
+      <body className={`${inter.className} antialiased bg-[url(../../public/Gradient.png)] bg-fixed`}>
         <div className="h-fit grid grid-cols-[1fr] grid-rows-[5rem_1fr]">
           <div className="row-start-1 row-end-2 col-start-1 col-end-2 flex justify-center items-center px-[0.5rem]">
             <Navbar />
